6650c7c76d1004f7f4ee4fc872cbc6b0fdd2361b
Minami Takayama <m.inam.ina03037@gmail.com>
2025-04-30 16:30:55 +0900

    fix point and player

--- src/components/board/Board.jsx
+++ src/components/board/Board.jsx
@@ -3,15 +3,18 @@ import Square from '../square/Square'
 import { useState } from "react";
 
 const Board = () => {
-  // set empty array called squares using useState so that it can be changed later
-  // (initial state =[null,null,null,...null])
-  const [squares, setSquares] = useState(Array(9).fill(null));
-  // set default next player (X) (use boolean, no need function)
+  // set an array for point of each squares, using useState so that it can be changed later
+  const [pointOfSquares, setPointOfSquares] = useState([10,10,10,0,0,0,-10,-10,-10]);
+  // set an array for player who get each squares,using useState so that it can be changed later [X, O, null,...]
+  const [playerGetSquares, setPlayerGetSquares] = useState(Array(9).fill(null));
+  // set variable for default next player (X) (use boolean, no need function)
   const [xIsNext, setXIsNext] = useState(true);
+  // set variable to astore initial score of the players
+  const [scoreX, setScoreX] = useState(0);
+  const [scoreO, setScoreO] = useState(0);
 
-  function whoIsWinner(){
-  // check if there's winner
-  // square[n] combinations decide winner
+  function bonusPoint(){
+  // square[n] combinations for tic-tac-toe
     const combinations = [
       [0, 1, 2],
       [3, 4, 5],
@@ -25,20 +28,18 @@ const Board = () => {
       [2, 4, 6],
     ];
 
-    // check squares value if they match the combination of game end
+    // check playerGetSquares[n](a,b,c-> places of square) if they match the combination
     for ( let i = 0; i < combinations.length; i++ ){
       // Destructuring（分割代入:配列の中身をまとめて変数に取り出す書き方）
       const [a, b, c] = combinations[i];
-      // value of square[a] is not empty
-      // value of squares[a]=squares[b]=squares[c] (a,b,c-> places of square)
-      if(squares[a] && squares[a]===squares[b] && squares[a]===squares[c]){
-        return  squares[a];
+      // playerGetSquares[a] is not empty and playerGetSquares[a]=[b]=[c]
+      if(playerGetSquares[a] && playerGetSquares[a]===playerGetSquares[b] && playerGetSquares[a]===playerGetSquares[c]){
+        return  playerGetSquares[a];
       }
     }
-    // check if draw |Array.every((x)x=>x===2);
-    // if every square has value, return draw.
-    if (squares.every((square) => square !== null)){
-      return "Draw"
+    // if every square has value, return 'no bonus point'.
+    if (playerGetSquares.every((square) => square !== null)){
+      return "No bonus point"
     }
     // otherwise no winner
     return null;
@@ -47,15 +48,15 @@ const Board = () => {
   // set variable status to display winner or next player
   let status;
   // set variable winner to store the result of whoIsWinner
-  const winner = whoIsWinner();
+  const bonus = bonusPoint();
 
   // display draw, winner, or next player
-  // if value of winner equals "Draw"
-  if (winner === "Draw"){
-    status = <h2><span>{winner}!</span></h2>
+  // if value of winner equals "No bonus point"
+  if (bonus === "No bonus point"){
+    return;
     //if winner seems truthy (X or O //not null, undefined...)
-  } else if (winner){
-    status = <h2><span>Winner is  {winner} !</span></h2>;
+  } else if (bonus){
+    status = <h2><span>{bonus} !</span></h2>;
   } else {
     status = <h2>Next Player is <span>{xIsNext ? 'X': 'O'}</span></h2>
   }
@@ -63,29 +64,41 @@ const Board = () => {
   // define function to handle the game
   function handleClick(n){
     console.log("handleClick called!", n);
-    // if squares[n] is already clicked || either one winned -> end the game
-    if (squares[n] !== null || whoIsWinner()){
+    // if all playerGetSquares are already got -> end the game
+    if (playerGetSquares[n] !== null){
       return;
     }
     // else (for empty squares) flip the clicked square
-    // copy previous array and set it as nextSquares (to create new one)
-    const nextSquares = squares.slice();
-    // update the value of n's square
-    nextSquares[n] = xIsNext ? "X" : "O";
+    // copy previous array and set it as nextPlayerGetSquares (to create new one)
+    const nextPlayerGetSquares = playerGetSquares.slice();
+    // set variable for current player and give it a value X or O
+    const currentPlayer = xIsNext ? "X" : "O";
+    // update the nextPlayerGetSquares with current player
+    nextPlayerGetSquares[n] = currentPlayer;
     // update the newly created array
-    setSquares(nextSquares);
+    setPlayerGetSquares(nextPlayerGetSquares);
+    // calculate the score of X,O
+    const point = pointOfSquares[n]
+    if(xIsNext){
+      setScoreX(scoreX + point);
+    } else{
+      setScoreO(scoreO + point);
+    }
     // set next player in turn for the next move
     setXIsNext(!xIsNext);
   }
 
   //define function to reset the game
   function resetSquares(){
-    setSquares(Array(9).fill(null));
+    setPointOfSquares(Array(9).fill(null));
     setXIsNext(true);
   }
 
   return (
     <>
+      <div>
+        Player1:{scoreX} - Player2: {scoreO}
+      </div>
       <div>
         {status}
       </div>
@@ -93,19 +106,19 @@ const Board = () => {
         {/* onSquareClick (which calls function handleClick) is given to Square as props,
             value of squares[n] is given to Square as props value */}
         <div>
-          <Square onSquareClick={() => handleClick(0)} value={squares[0]} />
-          <Square onSquareClick={() => handleClick(1)} value={squares[1]} />
-          <Square onSquareClick={() => handleClick(2)} value={squares[2]} />
+          <Square onSquareClick={() => handleClick(0)} point={pointOfSquares[0]} player={playerGetSquares[0]} />
+          <Square onSquareClick={() => handleClick(1)} point={pointOfSquares[0]} player={playerGetSquares[0]} />
+          <Square onSquareClick={() => handleClick(2)} point={pointOfSquares[0]} player={playerGetSquares[0]} />
         </div>
         <div>
-          <Square onSquareClick={() => handleClick(3)} value={squares[3]} />
-          <Square onSquareClick={() => handleClick(4)} value={squares[4]} />
-          <Square onSquareClick={() => handleClick(5)} value={squares[5]} />
+          <Square onSquareClick={() => handleClick(3)} point={pointOfSquares[0]} player={playerGetSquares[0]} />
+          <Square onSquareClick={() => handleClick(4)} point={pointOfSquares[0]} player={playerGetSquares[0]} />
+          <Square onSquareClick={() => handleClick(5)} point={pointOfSquares[0]} player={playerGetSquares[0]} />
         </div>
         <div>
-          <Square onSquareClick={() => handleClick(6)} value={squares[6]} />
-          <Square onSquareClick={() => handleClick(7)} value={squares[7]} />
-          <Square onSquareClick={() => handleClick(8)} value={squares[8]} />
+          <Square onSquareClick={() => handleClick(6)} point={pointOfSquares[0]} player={playerGetSquares[0]} />
+          <Square onSquareClick={() => handleClick(7)} point={pointOfSquares[0]} player={playerGetSquares[0]} />
+          <Square onSquareClick={() => handleClick(8)} point={pointOfSquares[0]} player={playerGetSquares[0]} />
         </div>
       </div>
       <div>
--- src/components/square/Square.jsx
+++ src/components/square/Square.jsx
@@ -1,10 +1,10 @@
 import './square.css';
 // get props {onSquareClick, value} from Board to display value when clicked
 // Square button with onClick -> props:onSquareClick is called -> onSquareClick calls function handleClick
-const Square = ({onSquareClick, value}) => {
+const Square = ({onSquareClick, point, player}) => {
   return (
       <button className="square-button" onClick={onSquareClick}>
-        {value}
+        {point}:{player}
       </button>
   );
 }
